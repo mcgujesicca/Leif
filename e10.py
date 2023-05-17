c43989fc3453a8187b44670d73a2ef94341c86b5
mcgujesicca <109402882+mcgujesicca@users.noreply.github.com>
2023-05-17 20:09:57 +0800

    Add files via upload

---
+++ e10.py
@@ -0,0 +1,11 @@
+jijie = input("请输入一个季节：")
+if jijie == "春":
+    print("1月2月3月")
+elif jijie == "夏":
+    print("4月5月6月")
+elif jijie == "秋":
+    print("7月8月9月")
+elif jijie == "冬":
+    print("10月11月12月")
+else: print("未知季节")
+
